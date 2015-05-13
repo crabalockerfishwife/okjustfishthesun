b77ad955782b810692789a1633266254e6373f6d
Fish Milnikiewicz <fmilniki@gmail.com>
2015-05-12 23:11:34 -0400

    nice styling and button works

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app=Flask(__name__)
 def index():
     return render_template("home.html")
 
+@app.route("/heat")
+def heat():
+    return render_template("heat.html")
+
 if __name__ == "__main__":
    app.debug = True
    app.run(host="0.0.0.0", port=8000)
