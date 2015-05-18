088acc347c55e092fd572c30be85b04df7da5b30
justinstrauss <justinianstrauss@gmail.com>
2015-05-18 12:19:35 -0400

    clean up

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 console.log("hello");
 
-var apis = ["Subway Entrances":"https://data.cityofnewyork.us/api/views/he7q-3hwy/rows.json?accessType=DOWNLOAD",
+var apis = {"Subway Entrances":"https://data.cityofnewyork.us/api/views/he7q-3hwy/rows.json?accessType=DOWNLOAD",
 		"NYPD Motor Vehicle Collisions":"https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.json?accessType=DOWNLOAD",
 		"Directory of Eateries":"https://data.cityofnewyork.us/api/views/xx67-kt59/rows.json?accessType=DOWNLOAD",
 		"Manhattan Street Trees":"https://data.cityofnewyork.us/api/views/4eh7-xcm8/rows.json?accessType=DOWNLOAD",
-		"Wifi Hotspots":"https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD"];
+		"Wifi Hotspots":"https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD"};
 //		"Pay Phones":"",
 //		"Museums and Galleries":"",
 //		"Theaters":"",
--- test.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello");
-
-link = "https://data.cityofnewyork.us/api/views/he7q-3hwy/rows.json?accessType=DOWNLOAD";
-$("data").html("testing");
