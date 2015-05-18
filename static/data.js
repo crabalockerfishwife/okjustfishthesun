5560d9787bdfa682d6bb73ae024d027dfaf7b5d1
justinstrauss <justinianstrauss@gmail.com>
2015-05-18 11:57:31 -0400

    data

---
+++ static/data.js
@@ -0,0 +1,26 @@
+apis = ["Subway Entrances":"https://data.cityofnewyork.us/api/views/he7q-3hwy/rows.json?accessType=DOWNLOAD",
+		"NYPD Motor Vehicle Collisions":"https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.json?accessType=DOWNLOAD",
+		"Directory of Eateries":"https://data.cityofnewyork.us/api/views/xx67-kt59/rows.json?accessType=DOWNLOAD",
+		"Manhattan Street Trees":"https://data.cityofnewyork.us/api/views/4eh7-xcm8/rows.json?accessType=DOWNLOAD",
+		"Wifi Hotspots":"https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD"];
+//		"Pay Phones":"",
+//		"Museums and Galleries":"",
+//		"Theaters":"",
+//		"Fields":"",
+//		"Swimming Pools":"",
+//		"Beaches":"",
+//		"Libraries":""];
+
+types = ["food","recreation","communication","transportation"];
+
+$.getJSON( "ajax/test.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    "class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
