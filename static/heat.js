b6bd22e6180816080558fbb6a69f601accd82202
Christina <christinako822@gmail.com>
2015-05-21 12:34:09 -0400

    fixing

--- static/heat.js
+++ static/heat.js
@@ -532,28 +532,7 @@ function initialize() {
     heatmap = new google.maps.visualization.HeatmapLayer({
 	data: pointArray
     });
-    //gradient changes the color of the heat map from green/red to blue/red
-    //I think the default gradient is green/red so remove the bottom part if you want that but leave heatmap.setMap(map)
     var gradient = [
-	// 'rgba(0, 255, 0, 0)',
-	// 'rgba(0, 255, 0, 1)',
-	// 'rgba(0, 191, 0, 1)',
-	// 'rgba(0, 127, 0, 1)',
-	// 'rgba(0, 63, 0, 1)',
-	// 'rgba(0, 255, 255, 1)',
-	// 'rgba(0, 255, 255, 1)',
-	// 'rgba(0, 191, 255, 1)',
-	// 'rgba(0, 127, 255, 1)',
-	// 'rgba(0, 63, 255, 1)',
-	// 'rgba(0, 0, 255, 1)',
-	// 'rgba(0, 0, 223, 1)',
-	// 'rgba(0, 0, 191, 1)',
-	// 'rgba(0, 0, 159, 1)',
-	// 'rgba(0, 0, 127, 1)',
-	// 'rgba(63, 0, 91, 1)',
-	// 'rgba(127, 0, 63, 1)',
-	// 'rgba(191, 0, 31, 1)',
-	// 'rgba(255, 0, 0, 1)'
 	'rgba(0,255,255,0)',
 	'rgba(0,255,255,1)',
 	'rgba(0,255,0,1)',
